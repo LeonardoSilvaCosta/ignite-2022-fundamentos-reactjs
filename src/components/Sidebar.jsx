2ad0b4422328382a04408915ee2fc8169f675d19
leonardocosta <sir.costa@yahoo.com.br>
2022-06-19 09:33:34 -0300

    Finalizando Sidebar, + lib phosphor-react

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +10,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/LeonardoSilvaCosta.png" />
+
         <strong>Leonardo Costa</strong>
         <span>Full Stack Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>

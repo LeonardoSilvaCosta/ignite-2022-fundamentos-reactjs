95874e648230e8740b666a5e63456de5a6855689
leonardocosta <sir.costa@yahoo.com.br>
2022-06-28 10:51:49 -0300

    Componente Avatar.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,18 @@
-import { PencilLine } from 'phosphor-react';
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
-import styles from './Sidebar.module.css';
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} 
-          src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/LeonardoSilvaCosta.png" />
+        <Avatar src="https://github.com/LeonardoSilvaCosta.png" alt="" />
 
         <strong>Leonardo Costa</strong>
         <span>Full Stack Developer</span>
